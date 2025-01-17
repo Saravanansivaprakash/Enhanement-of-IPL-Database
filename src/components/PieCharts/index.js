2e945d1b9a6485175d73da60e4da6f4ab68adcd8
saravanan <ssaravanan092@gmail.com>
2025-01-17 12:17:01 +0000

    updated

--- src/components/PieCharts/index.js
+++ src/components/PieCharts/index.js
@@ -7,22 +7,27 @@ const PieCharts = props => {
   const {data} = props
 
   return (
-    <PieChart width={290} height={350}>
-      <Pie
-        data={data}
-        innerRadius={0}
-        outerRadius={80}
-        dataKey="value"
-        nameKey="id"
-        label
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${entry.id}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip />
-      <Legend verticalAlign="bottom" height={36} />
-    </PieChart>
+    <div className="piechart-bg-container">
+      <PieChart width={290} height={350}>
+        <Pie
+          data={data}
+          innerRadius={0}
+          outerRadius={80}
+          dataKey="value"
+          nameKey="id"
+          label
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${entry.id}`}
+              fill={COLORS[index % COLORS.length]}
+            />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend verticalAlign="bottom" height={36} />
+      </PieChart>
+    </div>
   )
 }
 
